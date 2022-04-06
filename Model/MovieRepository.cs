e09c5a41c8a851b83fdff9b0cfc215e737e44dea
LaMarr Pace <lamarrpace@gmail.com>
2022-04-06 13:15:49 -0600

    removing test data

--- Model/MovieRepository.cs
+++ Model/MovieRepository.cs
@@ -10,9 +10,6 @@ namespace congaDemoApp.Model
 
         public MovieRepository()
         {
-            _movies.Add(new MovieModel() { ID = 1, Title = "SomeTitle", Review = "SomeReview", Rating = 5.5, ImgPath = "./image1.png" });
-            _movies.Add(new MovieModel() { ID = 2, Title = "SomeTitle1", Review = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", Rating = 6, ImgPath = "./image2.png" });
-            _movies.Add(new MovieModel() { ID = 3, Title = "SomeTitle2", Review = "SomeReview2", Rating = 9, ImgPath = "./image3.jpg" });
         }
 
         public IEnumerable<MovieModel> GetAll()
